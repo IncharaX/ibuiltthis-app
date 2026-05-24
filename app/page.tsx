67c9e4b1ad95c251e3dc68decd836251dc231569
Inchara nk <incharank1@gmail.com>
2026-05-25 00:28:28 +0530

    Project sarting

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello everyone!!</h1>
+      <h1>Share What You&apos;ve Built, Discover What&apos;s Launching</h1>
+      
+      
+      <Button size="lg">Click me!</Button>
     </div>
   );
 }
